3a0906654f5912f22e57c477e61038a0ac19e66d
SammRosenthal <samrrosenthal@gmail.com>
2019-05-12 17:43:08 -0600

    finished week1 course 008

--- mooc-2013-OOProgrammingWithJava-PART1/week1-008.Adder/src/Adder.java
+++ mooc-2013-OOProgrammingWithJava-PART1/week1-008.Adder/src/Adder.java
@@ -7,5 +7,12 @@ public class Adder {
         Scanner reader = new Scanner(System.in);
 
         // Implement your program here. Remember to ask the input from user
+
+        System.out.println("Type a number: ");
+        int firstNum = reader.nextInt();
+        System.out.println("Type another number: ");
+        int secondNum = reader.nextInt();
+
+        System.out.println("Sum of the numbers: " + (firstNum + secondNum));
     }
 }
