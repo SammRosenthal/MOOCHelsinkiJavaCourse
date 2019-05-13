fd9ab7cdeb0a6b286106a98df8bd6762525930ed
SammRosenthal <samrrosenthal@gmail.com>
2019-05-12 18:06:06 -0600

    finished week1 course 011

--- mooc-2013-OOProgrammingWithJava-PART1/week1-011.BiggerNumber/src/BiggerNumber.java
+++ mooc-2013-OOProgrammingWithJava-PART1/week1-011.BiggerNumber/src/BiggerNumber.java
@@ -7,5 +7,16 @@ public class BiggerNumber {
         Scanner reader = new Scanner(System.in);
 
         // Implement your program here. Remember to ask the input from user
+
+        System.out.println("Type a number: ");
+        int firstNum = reader.nextInt();
+        System.out.println("Type another number: ");
+        int secondNum = reader.nextInt();
+
+        if (firstNum > secondNum) {
+            System.out.println("The bigger number of the two numbers given was: " + firstNum);
+        } else {
+            System.out.println("The bigger number of the two numbers given was: " + secondNum);
+        }
     }
 }
