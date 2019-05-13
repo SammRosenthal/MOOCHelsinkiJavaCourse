f16828878a6d79486a01cd4dfca76c6911bb1751
SammRosenthal <samrrosenthal@gmail.com>
2019-05-12 18:15:20 -0600

    finished week1 course 012

--- mooc-2013-OOProgrammingWithJava-PART1/week1-012.SumOfTheAges/src/SumOfTheAges.java
+++ mooc-2013-OOProgrammingWithJava-PART1/week1-012.SumOfTheAges/src/SumOfTheAges.java
@@ -7,5 +7,16 @@ public class SumOfTheAges {
         Scanner reader = new Scanner(System.in);
 
         // Implement your program here
+        System.out.println("Type your name: ");
+        String firstName = reader.nextLine();
+        System.out.println("Type your age: ");
+        int firstAge = reader.nextInt();
+        System.out.println("Type your name: " );
+        reader.nextLine();
+        String secondName = reader.nextLine();
+        System.out.println("Type your age: ");
+        int secondAge = reader.nextInt();
+
+        System.out.println(firstName + " and " + secondName + " are " + (firstAge + secondAge) + " years old in total.");
     }
 }
