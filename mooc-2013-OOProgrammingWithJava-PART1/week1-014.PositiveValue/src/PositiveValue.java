64540877ef15aa9179b1b028b5c5a9eb45eb6b57
SammRosenthal <samrrosenthal@gmail.com>
2019-05-13 11:34:26 -0500

    finished week1 course 014

--- mooc-2013-OOProgrammingWithJava-PART1/week1-014.PositiveValue/src/PositiveValue.java
+++ mooc-2013-OOProgrammingWithJava-PART1/week1-014.PositiveValue/src/PositiveValue.java
@@ -6,6 +6,14 @@ public class PositiveValue {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
-        // Type your program here:
+        System.out.println("Type a number: ");
+
+        int number = reader.nextInt();
+
+        if (number > 0) {
+            System.out.println("Your number is positive.");
+        } else {
+            System.out.println("Your number is not positive.");
+        }
     }
 }
