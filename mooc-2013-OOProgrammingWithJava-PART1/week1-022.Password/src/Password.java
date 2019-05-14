6459990aec2195edeae65b992a1df88f7fcb547d
SammRosenthal <samrrosenthal@gmail.com>
2019-05-14 12:55:51 -0500

    finished week1 course 022

--- mooc-2013-OOProgrammingWithJava-PART1/week1-022.Password/src/Password.java
+++ mooc-2013-OOProgrammingWithJava-PART1/week1-022.Password/src/Password.java
@@ -8,5 +8,19 @@ public class Password {
         String password = "carrot"; // Use carrot as password when running tests.
 
         // Write your code here
+
+        while(true){
+            System.out.println("Type the password: ");
+            String pass = reader.nextLine();
+
+            if (pass.equals("carrot")){
+                System.out.println("Right!");
+                break;
+            } else {
+                System.out.println("Wrong!");
+            }
+        }
+
+        System.out.println("The secret is: jryy qbar!");
     }
 }
