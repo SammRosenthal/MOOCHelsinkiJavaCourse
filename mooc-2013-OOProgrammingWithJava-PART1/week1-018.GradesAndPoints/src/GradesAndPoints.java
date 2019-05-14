2f78100ffd16f684b8cf54bc4dfc8b02e1c5370c
SammRosenthal <samrrosenthal@gmail.com>
2019-05-13 20:04:58 -0500

    week 1 course 019

--- mooc-2013-OOProgrammingWithJava-PART1/week1-018.GradesAndPoints/src/GradesAndPoints.java
+++ mooc-2013-OOProgrammingWithJava-PART1/week1-018.GradesAndPoints/src/GradesAndPoints.java
@@ -6,5 +6,22 @@ public class GradesAndPoints {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
+        System.out.println("Type the points [0-60]: ");
+        int points = reader.nextInt();
+
+        if (points < 30) {
+            System.out.println("Grade: failed");
+        } else if ( points < 35) {
+            System.out.println("Grade: 1");
+        } else if (points < 40) {
+            System.out.println("Grade: 2");
+        } else if (points < 45) {
+            System.out.println("Grade: 3");
+        } else if (points < 50) {
+            System.out.println("Grade: 4");
+        } else if (points < 61) {
+            System.out.println("Grade: 5");
+        }
+
     }
 }
