cb2d125363e6955992603958756e9e62ce0f8a7b
SammRosenthal <samrrosenthal@gmail.com>
2019-05-14 13:13:42 -0500

    finished week1 course 024

--- mooc-2013-OOProgrammingWithJava-PART1/week1-024.NhlStatisticsPart2/src/NhlStatisticsPart2.java
+++ mooc-2013-OOProgrammingWithJava-PART1/week1-024.NhlStatisticsPart2/src/NhlStatisticsPart2.java
@@ -1,6 +1,7 @@
 
-import java.util.Scanner;
 import nhlstats.NHLStatistics;
+
+import java.util.Scanner;
 
 public class NhlStatisticsPart2 {
 
@@ -19,18 +20,32 @@ public class NhlStatisticsPart2 {
             }
 
             if (command.equals("points")) {
-                // Print the top ten players sorted by points.
+                NHLStatistics.sortByPoints();
+                NHLStatistics.top(10);
             } else if (command.equals("goals")) {
                 // Print the top ten players sorted by goals.
+                NHLStatistics.sortByGoals();
+                NHLStatistics.top(10);
             } else if (command.equals("assists")) {
                 //  Print the top ten players sorted by assists.
+                NHLStatistics.sortByAssists();
+                NHLStatistics.top(10);
             } else if (command.equals("penalties")) {
                 //  Print the top ten players sorted by penalties.
+                NHLStatistics.sortByPenalties();
+                NHLStatistics.top(10);
             } else if (command.equals("player")) {
                 // Ask the user first which player's statistics are needed and then print them.
+                System.out.println("What player: ");
+                String player = reader.nextLine();
+                NHLStatistics.searchByPlayer(player);
             } else if (command.equals("club")) {
                 // Ask the user first which club's statistics are needed and then print them.
                 // Note: When printing statistics they should be ordered by points (so the players with the most points come first).
+                NHLStatistics.sortByPoints();
+                System.out.println("What club: ");
+                String club = reader.nextLine();
+                NHLStatistics.teamStatistics(club);
             }
 
         }
