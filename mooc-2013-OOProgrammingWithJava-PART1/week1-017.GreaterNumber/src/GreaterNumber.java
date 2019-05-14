a081bbf2d7749ac95535861151d36f07a9f454ec
SammRosenthal <samrrosenthal@gmail.com>
2019-05-13 19:59:32 -0500

    finished week 1 course 017

--- mooc-2013-OOProgrammingWithJava-PART1/week1-017.GreaterNumber/src/GreaterNumber.java
+++ mooc-2013-OOProgrammingWithJava-PART1/week1-017.GreaterNumber/src/GreaterNumber.java
@@ -5,5 +5,19 @@ public class GreaterNumber {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
+        System.out.println("Type the first number: ");
+        int first = reader.nextInt();
+
+        System.out.println("Type hte second number: ");
+        int second = reader.nextInt();
+
+        if (first == second) {
+            System.out.println("The numbers are equal!");
+        } else if (first > second) {
+            System.out.println("Greater number: " + first);
+        } else if (second > first) {
+            System.out.println("Greater number: " + second);
+        }
+
     }
 }
