a560f378697e9268422814484115eda884a0e984
SammRosenthal <samrrosenthal@gmail.com>
2019-05-14 13:02:45 -0500

    finished week 1 course 023

--- mooc-2013-OOProgrammingWithJava-PART1/week1-023.Temperatures/src/Temperatures.java
+++ mooc-2013-OOProgrammingWithJava-PART1/week1-023.Temperatures/src/Temperatures.java
@@ -9,11 +9,23 @@ public class Temperatures {
         // Write your code here. 
 
         // Graph is used as follows:
-        Graph.addNumber(7);
-        double value = 13.5;
-        Graph.addNumber(value);
-        value = 3;
-        Graph.addNumber(value);
+//        Graph.addNumber(7);
+//        double value = 13.5;
+//        Graph.addNumber(value);
+//        value = 3;
+//        Graph.addNumber(value);
         // Remove or comment out these lines above before trying to run the tests.
+
+        while(true){
+            System.out.println("Insert temperature as floating point number: ");
+            double temp = Double.parseDouble(reader.nextLine());
+
+            if (temp > 40 || temp < -30){
+                System.out.println("That is not a valid temperature.");
+            } else {
+                Graph.addNumber(temp);
+            }
+        }
+
     }
 }
