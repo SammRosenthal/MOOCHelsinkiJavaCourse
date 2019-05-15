8c64f28bd8727b640a3f82a36f9485d6458c79dc
SammRosenthal <samrrosenthal@gmail.com>
2019-05-15 13:20:52 -0500

    finished week2 course 030

--- mooc-2013-OOProgrammingWithJava-PART1/week2-030.UpToCertainNumber/src/UpToCertainNumber.java
+++ mooc-2013-OOProgrammingWithJava-PART1/week2-030.UpToCertainNumber/src/UpToCertainNumber.java
@@ -8,6 +8,14 @@ public class UpToCertainNumber {
         Scanner reader = new Scanner(System.in);
         
         // Write your code here
+        System.out.println("Up to what number?");
+        int userInt = reader.nextInt();
+        int num = 1;
+
+        while (num <= userInt) {
+            System.out.println(num);
+            num++;
+        }
         
     }
 }
