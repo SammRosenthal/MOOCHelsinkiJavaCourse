9e05ef25d1b1392cce034da7b6ccfd5d86de195b
SammRosenthal <samrrosenthal@gmail.com>
2019-05-15 13:41:13 -0500

    finished week2 course 035

--- mooc-2013-OOProgrammingWithJava-PART1/week2-035.SumOfThePowers/src/SumOfThePowers.java
+++ mooc-2013-OOProgrammingWithJava-PART1/week2-035.SumOfThePowers/src/SumOfThePowers.java
@@ -6,5 +6,17 @@ public class SumOfThePowers {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
+        System.out.println("Enter a number: ");
+        int num = reader.nextInt();
+
+        int result = 0;
+
+        while (num >= 0) {
+            result += (int)Math.pow(2, num);
+            num--;
+        }
+
+
+        System.out.println("The result is: " + result);
     }
 }
