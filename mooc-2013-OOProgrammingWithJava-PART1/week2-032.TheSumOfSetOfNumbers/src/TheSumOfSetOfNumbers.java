82aad5a8bc77ff9d8c92a357e6170f3f8f4d5930
SammRosenthal <samrrosenthal@gmail.com>
2019-05-15 13:28:22 -0500

    finished week2 course 032

--- mooc-2013-OOProgrammingWithJava-PART1/week2-032.TheSumOfSetOfNumbers/src/TheSumOfSetOfNumbers.java
+++ mooc-2013-OOProgrammingWithJava-PART1/week2-032.TheSumOfSetOfNumbers/src/TheSumOfSetOfNumbers.java
@@ -6,5 +6,15 @@ public class TheSumOfSetOfNumbers {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
+        System.out.println("Until what?");
+        int userInput = reader.nextInt();
+        int sum = 0;
+
+        while (userInput >= 1) {
+            sum += userInput;
+            userInput--;
+        }
+
+        System.out.println("Sum is " + sum);
     }
 }
