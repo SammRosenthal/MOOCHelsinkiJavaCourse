d2463ea0a0de0f2aa30b94e59b096d9e592df3e3
SammRosenthal <samrrosenthal@gmail.com>
2019-05-15 15:28:09 -0500

    finished week2 course 039

--- mooc-2013-OOProgrammingWithJava-PART1/week2-039.Printing/src/Printing.java
+++ mooc-2013-OOProgrammingWithJava-PART1/week2-039.Printing/src/Printing.java
@@ -1,27 +1,87 @@
+//public class Printing {
+//
+//    public static void printStars(int amount) {
+//        // 39.1
+//        // you can print one star with the command
+//        // System.out.print("*");
+//        // call this command amount times
+//
+//        while (amount > 0) {
+//            System.out.println("*");
+//            amount--;
+//        }
+//    }
+//
+//    public static void printSquare(int sideSize) {
+//        // 39.2
+//    }
+//
+//    public static void printRectangle(int width, int height) {
+//        // 39.3
+//    }
+//
+//    public static void printTriangle(int size) {
+//        // 39.4
+//    }
+//
+//    public static void main(String[] args) {
+//        // Tests do not use main, yo can write code here freely!
+//        // if you have problems with tests, please try out first
+//        // here to see that the printout looks correct
+//
+//        printStars(3);
+//        System.out.println("\n---");  // printing --- to separate the figures
+//        printSquare(4);
+//        System.out.println("\n---");
+//        printRectangle(5, 6);
+//        System.out.println("\n---");
+//        printTriangle(3);
+//        System.out.println("\n---");
+//    }
+//
+//}
+
 public class Printing {
 
     public static void printStars(int amount) {
+        int i =0;
+        while (i<amount){
+            System.out.print("*");
+            i++;
+        }
+
+        System.out.println();
         // 39.1
         // you can print one star with the command
         // System.out.print("*");
-        // call this command amount times        
+        // call this command amount times
     }
 
     public static void printSquare(int sideSize) {
+        for (int i = 0; i < sideSize; i++){
+            printStars(sideSize);
+        }
+
         // 39.2
     }
 
     public static void printRectangle(int width, int height) {
         // 39.3
+        for (int i = 0; i < height; i++){
+            printStars(width);
+        }
+
     }
 
     public static void printTriangle(int size) {
-        // 39.4
+        for (int i = 1; i <= size; i++){
+            printStars(i);
+        }
     }
 
     public static void main(String[] args) {
         // Tests do not use main, yo can write code here freely!
-        // if you have problems with tests, please try out first 
+        // if you have problems with tests, please try out first
         // here to see that the printout looks correct
 
         printStars(3);
