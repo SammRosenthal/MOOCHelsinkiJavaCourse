fd4a803bdfb9310771c440ccfb8f0bcd4d900ee9
SammRosenthal <samrrosenthal@gmail.com>
2019-05-15 13:34:43 -0500

    finished week2 course 034

--- mooc-2013-OOProgrammingWithJava-PART1/week2-034.Factorial/src/Factorial.java
+++ mooc-2013-OOProgrammingWithJava-PART1/week2-034.Factorial/src/Factorial.java
@@ -4,5 +4,21 @@ public class Factorial {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
+        System.out.println("Type a number: ");
+        int num = reader.nextInt();
+
+        int factorial = 1;
+
+        while (num > 0) {
+            factorial *= num;
+            num--;
+        }
+
+        if (factorial == 0) {
+            System.out.println("Factorial is 1");
+        } else {
+            System.out.println("Factorial is " + factorial);
+        }
+
     }
 }
