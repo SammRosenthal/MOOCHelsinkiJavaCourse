1821caa0fc3defb91067b275e3b202a99b71d4ac
SammRosenthal <samrrosenthal@gmail.com>
2019-05-15 13:52:42 -0500

    finished week2 course 36

--- mooc-2013-OOProgrammingWithJava-PART1/week2-036.LoopsEndingRemembering/src/LoopsEndingRemembering.java
+++ mooc-2013-OOProgrammingWithJava-PART1/week2-036.LoopsEndingRemembering/src/LoopsEndingRemembering.java
@@ -7,5 +7,38 @@ public class LoopsEndingRemembering {
         
         Scanner reader = new Scanner(System.in);
 
+        int sum = 0;
+        int count = 0;
+        int even = 0;
+        int odd = 0;
+
+
+        while (true) {
+            System.out.println("Type numbers: ");
+            int num = reader.nextInt();
+
+            if (num == -1) {
+                break;
+            }
+
+            if (num % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+
+            sum += num;
+            count++;
+        }
+
+        double average = (double)sum / count;
+
+        System.out.println("Thank you and see you later!");
+        System.out.println("The sum is " + sum);
+        System.out.println("How many numbers: " + count);
+        System.out.println("Average: " + average);
+        System.out.println("Even numbers: " + even);
+        System.out.println("Odd numbers: " + odd);
+
     }
 }
