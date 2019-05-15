8aeeb068a7e6cb481a35615011186fbdc7aa8861
SammRosenthal <samrrosenthal@gmail.com>
2019-05-15 13:31:08 -0500

    finished week2 course 033

--- mooc-2013-OOProgrammingWithJava-PART1/week2-033.TheSumBetweenTwoNumbers/src/TheSumBetweenTwoNumbers.java
+++ mooc-2013-OOProgrammingWithJava-PART1/week2-033.TheSumBetweenTwoNumbers/src/TheSumBetweenTwoNumbers.java
@@ -5,6 +5,20 @@ import java.util.Scanner;
 public class TheSumBetweenTwoNumbers {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+
+        System.out.println("First: ");
+        int first = reader.nextInt();
+        System.out.println("Last: ");
+        int last = reader.nextInt();
+
+        int sum = 0;
+
+        while (first <= last) {
+            sum += first;
+            first++;
+        }
+
+        System.out.println("Sum is: " + sum);
         
     }
 }
