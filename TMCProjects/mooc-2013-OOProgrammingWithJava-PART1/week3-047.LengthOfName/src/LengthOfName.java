7144fe6983f7b472fa1b424fdb3563645c9933c7
Sam Rosenthal <samrrosenthal@gmail.com>
2019-05-16 16:58:52 -0700

    finished week3 course 047

--- TMCProjects/mooc-2013-OOProgrammingWithJava-PART1/week3-047.LengthOfName/src/LengthOfName.java
+++ TMCProjects/mooc-2013-OOProgrammingWithJava-PART1/week3-047.LengthOfName/src/LengthOfName.java
@@ -5,9 +5,13 @@ public class LengthOfName {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         // call your method from here
+        int length = calculateCharacters("Pekka");
+        System.out.println("Number of characters: " + length);
     }
     
     // do here the method
-    // public static int calculateCharacters(String text)
+     public static int calculateCharacters(String text) {
+        return text.length();
+     }
     
 }
