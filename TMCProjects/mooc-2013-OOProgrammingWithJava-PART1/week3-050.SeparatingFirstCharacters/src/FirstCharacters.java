6ae039c05559853d947b1fca55b2aa1a06e86a10
Sam Rosenthal <samrrosenthal@gmail.com>
2019-05-17 10:52:01 -0700

    finished week3 course 050

--- TMCProjects/mooc-2013-OOProgrammingWithJava-PART1/week3-050.SeparatingFirstCharacters/src/FirstCharacters.java
+++ TMCProjects/mooc-2013-OOProgrammingWithJava-PART1/week3-050.SeparatingFirstCharacters/src/FirstCharacters.java
@@ -3,5 +3,17 @@ import java.util.Scanner;
 public class FirstCharacters {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+
+        System.out.println("Type your name: ");
+        String name = reader.nextLine();
+
+        int count = 0;
+
+        if (name.length() > 2) {
+            while (count < 3) {
+                System.out.println((count + 1) + ". character: " + (name.charAt(count)));
+                count++;
+            }
+        }
     }
 }
