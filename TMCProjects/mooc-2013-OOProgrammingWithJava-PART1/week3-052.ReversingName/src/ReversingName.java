eaf4f3b32d39b6339e3a162c678eddecc1dcd52b
Sam Rosenthal <samrrosenthal@gmail.com>
2019-05-17 11:05:53 -0700

    finished week 3 course 052

--- TMCProjects/mooc-2013-OOProgrammingWithJava-PART1/week3-052.ReversingName/src/ReversingName.java
+++ TMCProjects/mooc-2013-OOProgrammingWithJava-PART1/week3-052.ReversingName/src/ReversingName.java
@@ -3,5 +3,16 @@ import java.util.Scanner;
 public class ReversingName {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+
+        System.out.println("Type your name: ");
+        String name = reader.nextLine();
+        String reversed = "";
+        int count = name.length() - 1;
+
+        while (count >= 0) {
+            reversed += name.charAt(count);
+            count--;
+        }
+        System.out.println("In reverse order: " + reversed);
     }
 }
