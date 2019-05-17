7b15640c36a834ac607afa439435ace34b2b40d7
Sam Rosenthal <samrrosenthal@gmail.com>
2019-05-16 17:02:04 -0700

    finished week3 course 048

--- TMCProjects/mooc-2013-OOProgrammingWithJava-PART1/week3-048.FirstCharacter/src/FirstCharacter.java
+++ TMCProjects/mooc-2013-OOProgrammingWithJava-PART1/week3-048.FirstCharacter/src/FirstCharacter.java
@@ -4,5 +4,12 @@ public class FirstCharacter {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        System.out.println("Type your name: ");
+
+        System.out.println("First character: " + firstCharacter(reader.next()));
+    }
+
+    public static char firstCharacter(String text) {
+        return text.charAt(0);
     }
 }
