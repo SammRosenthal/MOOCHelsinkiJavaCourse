6b8875158fef634a25bf26ae882d4568db03ee2f
Sam Rosenthal <samrrosenthal@gmail.com>
2019-05-18 13:13:57 -0700

    finished week 3 course 059

--- TMCProjects/mooc-2013-OOProgrammingWithJava-PART1/week3-059.WordsInReverseOrder/src/WordsInReverseOrder.java
+++ TMCProjects/mooc-2013-OOProgrammingWithJava-PART1/week3-059.WordsInReverseOrder/src/WordsInReverseOrder.java
@@ -6,5 +6,23 @@ public class WordsInReverseOrder {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         // create here an ArrayList
+        ArrayList<String> words = new ArrayList<String>();
+
+
+        while (true) {
+            System.out.println("Type a word: ");
+            String word = reader.nextLine();
+
+            if (word.isEmpty()) {
+                System.out.println("You typed the following words: ");
+                Collections.reverse(words);
+                for (String i : words) {
+                    System.out.println(i);
+                }
+                break;
+            } else {
+                words.add(word);
+            }
+        }
     }
 }
--- TMCProjects/mooc-2013-OOProgrammingWithJava-PART1/week3-060.WordsInAlphabeticalOrder/src/WordsInAlphabeticalOrder.java
+++ TMCProjects/mooc-2013-OOProgrammingWithJava-PART1/week3-060.WordsInAlphabeticalOrder/src/WordsInAlphabeticalOrder.java
@@ -1,4 +1,3 @@
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
@@ -8,5 +7,22 @@ public class WordsInAlphabeticalOrder {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         // create here an ArrayList
+        ArrayList<String> words = new ArrayList<String>();
+
+        while(true){
+            System.out.println("Type a word: ");
+            String word = reader.nextLine();
+
+            if (word.isEmpty()){
+                Collections.sort(words);
+                System.out.println("You typed teh following words: ");
+                for (String i : words) {
+                    System.out.println(i);
+                }
+                break;
+            } else {
+                words.add(word);
+            }
+        }
     }
 }
