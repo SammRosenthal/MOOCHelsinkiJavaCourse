248e486643d85edc274f7520d816525be5169029
Sam Rosenthal <samrrosenthal@gmail.com>
2019-05-20 15:34:23 -0700

    finished week 3 course 061

--- TMCProjects/mooc-2013-OOProgrammingWithJava-PART1/week3-061.NumberOfItems/src/NumberOfItems.java
+++ TMCProjects/mooc-2013-OOProgrammingWithJava-PART1/week3-061.NumberOfItems/src/NumberOfItems.java
@@ -4,6 +4,10 @@ public class NumberOfItems {
 
     // implement here the method countItems
 
+    public static int countItems(ArrayList<String> list) {
+        return list.size();
+    }
+
     public static void main(String[] args) {
         ArrayList<String> list = new ArrayList<String>();
         list.add("Moi");
@@ -11,7 +15,9 @@ public class NumberOfItems {
         list.add("Hello");
         System.out.println("There are this many items on the list:");
         // You can remove the comment from below when the method is done
-        //System.out.println(countItems(list)); 
+        System.out.println(countItems(list));
+
+
     }
 
 }
