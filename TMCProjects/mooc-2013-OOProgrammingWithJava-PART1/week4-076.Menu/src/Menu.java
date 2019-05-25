7f97c725421c39d0ec394a2b180c0e9ca23c0804
SammRosenthal <samrrosenthal@gmail.com>
2019-05-25 08:47:26 -0600

    finished week 4 course 076

--- TMCProjects/mooc-2013-OOProgrammingWithJava-PART1/week4-076.Menu/src/Menu.java
+++ TMCProjects/mooc-2013-OOProgrammingWithJava-PART1/week4-076.Menu/src/Menu.java
@@ -10,4 +10,21 @@ public class Menu {
     }
 
     // add the methods here
+
+    public void addMeal (String meal) {
+
+        if (!this.meals.contains(meal)) {
+            this.meals.add(meal);
+        }
+    }
+
+    public void printMeals () {
+        for (String i : this.meals) {
+            System.out.println(i);
+        }
+    }
+
+    public void clearMenu () {
+        this.meals.clear();
+    }
 }
