ce380c32b9080761b82498980cf3a803ccebab4c
SammRosenthal <samrrosenthal@gmail.com>
2019-05-27 20:23:30 -0500

    finished week 4 course 081

--- TMCProjects/mooc-2013-OOProgrammingWithJava-PART1/week4-081.PasswordRandomizer/src/PasswordRandomizer.java
+++ TMCProjects/mooc-2013-OOProgrammingWithJava-PART1/week4-081.PasswordRandomizer/src/PasswordRandomizer.java
@@ -2,13 +2,29 @@ import java.util.Random;
 
 public class PasswordRandomizer {
     // Define the variables
+    private int length;
+    Random charFinder = new Random();
 
     public PasswordRandomizer(int length) {
         // Initialize the variable
+        this.length = length;
     }
 
     public String createPassword() {
         // write code that returns a randomized password
-        return "";
+
+        int count = 0;
+        String pass = "";
+
+        while (count < this.length) {
+            String symbol = "abcdefghijklmnopqrstuvwxyz";
+            int index = charFinder.nextInt(26);
+            char currentChar = symbol.charAt(index);
+            System.out.println("currentChar - " + symbol.charAt(index));
+            pass += currentChar;
+            count++;
+        }
+        System.out.println(pass);
+        return pass;
     }
 }
