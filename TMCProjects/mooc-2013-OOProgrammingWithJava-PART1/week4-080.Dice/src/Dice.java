82594aeb8bf9010f070230641b4d3538e0de5a93
SammRosenthal <samrrosenthal@gmail.com>
2019-05-27 20:03:48 -0500

    finished week 4 course 080

--- TMCProjects/mooc-2013-OOProgrammingWithJava-PART1/week4-080.Dice/src/Dice.java
+++ TMCProjects/mooc-2013-OOProgrammingWithJava-PART1/week4-080.Dice/src/Dice.java
@@ -6,10 +6,11 @@ public class Dice {
 
     public Dice(int numberOfSides) {
         // Initialize here the number of sides
+        this.numberOfSides = numberOfSides;
     }
 
     public int roll() {
-        // create here a random number belongig to range 1-numberOfSided
-        return 0;
+        // create here a random number belonging to range 1-numberOfSided
+        return (random.nextInt(this.numberOfSides) + 1);
     }
 }
