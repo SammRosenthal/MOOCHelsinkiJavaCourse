29fb00fca5efa60e23648ee109a112a55e5b2f77
SammRosenthal <samrrosenthal@gmail.com>
2019-06-03 15:03:25 -0500

    finished week 5 cou course 086

--- TMCProjects/mooc-2013-OOProgrammingWithJava-PART1/week5-086.LyyraCardAndRegister/src/CashRegister.java
+++ TMCProjects/mooc-2013-OOProgrammingWithJava-PART1/week5-086.LyyraCardAndRegister/src/CashRegister.java
@@ -6,8 +6,41 @@ public class CashRegister {
     private int gourmetSold;
 
     public CashRegister() {
-        // at start the register has 1000 euros 
+        // at start the register has 1000 euros
+        this.cashInRegister = 1000.00;
     }
+
+    public boolean payEconomical(LyyraCard card) {
+        // the price of the economical lunch is 2.50 euros
+        // if the balance of the card is at least the price of the lunch:
+        //    the amount of sold lunches is incremented by one
+        //    the method returns true
+        // if not, the method returns false
+
+        if (card.balance() >= 2.50) {
+            card.pay(2.50);
+            this.economicalSold++;
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean payGourmet(LyyraCard card) {
+        // the price of the gourmet lunch is 4.00 euros
+        // if the balance of the card is at least the price of the lunch:
+        //    the amount of sold lunches is incremented by one
+        //    the method returns true
+        // if not, the method returns false
+
+        if (card.balance() >= 4) {
+            card.pay(4);
+            this.gourmetSold++;
+            return true;
+        }
+        return false;
+    }
+
 
     public double payEconomical(double cashGiven) {
         // price of the economical lunch is 2.50 euros
@@ -15,8 +48,14 @@ public class CashRegister {
         //    the price of lunch is added to register
         //    the amount of sold lunch is incremented by one
         //    method returns cashGiven - lunch price 
-        // if not enough money given, all is returned and nothing else happens        
-        return -1;
+        // if not enough money given, all is returned and nothing else happens
+
+        if (cashGiven >= 2.50) {
+            this.cashInRegister += 2.50;
+            this.economicalSold++;
+            return cashGiven - 2.50;
+        }
+        return cashGiven;
     }
 
     public double payGourmet(double cashGiven) {
@@ -26,7 +65,21 @@ public class CashRegister {
         //    the amount of sold lunch is incremented by one
         //    method returns cashGiven - lunch price 
         // if not enough money given, all is returned and nothing else happens
-        return -1;
+
+        if (cashGiven >= 4) {
+            this.cashInRegister += 4;
+            this.gourmetSold++;
+            return cashGiven - 4;
+        }
+        return cashGiven;
+
+    }
+
+    public void loadMoneyToCard(LyyraCard card, double sum) {
+        if (sum > 0) {
+            this.cashInRegister += sum;
+            card.loadMoney(sum);
+        }
     }
 
 
