4071a09920fe983a5bc9cdde28099392baa52db3
SammRosenthal <samrrosenthal@gmail.com>
2019-06-03 14:50:07 -0500

    finished week 5 cou course 085

--- TMCProjects/mooc-2013-OOProgrammingWithJava-PART1/week5-085.Reformatory/src/Reformatory.java
+++ TMCProjects/mooc-2013-OOProgrammingWithJava-PART1/week5-085.Reformatory/src/Reformatory.java
@@ -1,8 +1,24 @@
 public class Reformatory {
 
+    private int measured;
+
+    public Reformatory () {
+        this.measured = 0;
+    }
+
     public int weight(Person person) {
         // return the weight of the person
-        return -1;
+        this.measured++;
+        return person.getWeight();
+
+    }
+
+    public void feed(Person person) {
+        person.setWeight(person.getWeight() + 1);
+    }
+
+    public int totalWeightsMeasured() {
+        return this.measured;
     }
 
 }
