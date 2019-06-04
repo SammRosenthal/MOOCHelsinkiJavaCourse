07835085aeb4410b391c4309317e867573c37468
Sam Rosenthal <samrrosenthal@gmail.com>
2019-06-04 12:22:51 -0500

    finished week 5 course 089

--- TMCProjects/mooc-2013-OOProgrammingWithJava-PART1/week5-089.ClockObject/src/BoundedCounter.java
+++ TMCProjects/mooc-2013-OOProgrammingWithJava-PART1/week5-089.ClockObject/src/BoundedCounter.java
@@ -1,4 +1,42 @@
-
 public class BoundedCounter {
-    // copy here the class BoundedCounter from last weeks assignment 78 
+
+    private int value;
+    private int upperLimit;
+
+    public BoundedCounter(int upperLimit) {
+        this.upperLimit = upperLimit;
+    }
+    public BoundedCounter(int value, int upperLimit) {
+        this.value = value;
+        this.upperLimit = upperLimit;
+    }
+
+    public void next () {
+        this.value++;
+
+        if (this.value > this.upperLimit) {
+            this.value = 0;
+        }
+    }
+
+    public void setValue (int value) {
+        if (value < 0 || value > this.upperLimit) {
+            this.value = 0;
+        } else {
+            this.value = value;
+        }
+    }
+
+    public int getValue () {
+        return this.value;
+    }
+
+    public String toString () {
+        if (this.value < 10) {
+            return "0" + this.value;
+        } else {
+            return "" + this.value;
+        }
+
+    }
 }
--- TMCProjects/mooc-2013-OOProgrammingWithJava-PART1/week5-089.ClockObject/src/Clock.java
+++ TMCProjects/mooc-2013-OOProgrammingWithJava-PART1/week5-089.ClockObject/src/Clock.java
@@ -5,14 +5,28 @@ public class Clock {
    
     public Clock(int hoursAtBeginning, int minutesAtBeginning, int secondsAtBeginning) {
         // the counters that represent hours, minutes and seconds are created and set to have the correct initial values
+        this.hours = new BoundedCounter(hoursAtBeginning, 23);
+        this.minutes = new BoundedCounter(minutesAtBeginning, 59);
+        this.seconds = new BoundedCounter(secondsAtBeginning, 59);
     }
     
     public void tick() {
-        // Clock advances by one second
+        this.seconds.next();
+
+        if (this.seconds.getValue() == 0) {
+            this.minutes.next();
+
+            // if minutes become zero, advance hours
+            if (this.minutes.getValue() == 0) {
+                this.hours.next();
+            }
+        }
+
+
     }
     
     public String toString() {
         // returns the string representation
-        return "";
+        return this.hours + ":" + this.minutes + ":" + this.seconds;
     }
 }
