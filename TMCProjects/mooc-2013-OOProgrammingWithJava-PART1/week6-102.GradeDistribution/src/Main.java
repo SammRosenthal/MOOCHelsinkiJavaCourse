0754f7cbed82c2e8dcd5a174c2eaf68fd0f7b407
Sam Rosenthal <samrrosenthal@gmail.com>
2019-06-15 12:45:50 -0500

    finished makign a grade calc and reporter

--- TMCProjects/mooc-2013-OOProgrammingWithJava-PART1/week6-102.GradeDistribution/src/Main.java
+++ TMCProjects/mooc-2013-OOProgrammingWithJava-PART1/week6-102.GradeDistribution/src/Main.java
@@ -2,11 +2,27 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner lukija = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        Grades grades = new Grades();
         // implement your program here
         // do not put all to one method/class but rather design a proper structure to your program
         
         // Your program should use only one Scanner object, i.e., it is allowed to call 
         // new Scanner only once. If you need scanner in multiple places, you can pass it as parameter
+
+
+        System.out.println("Type exam scores, -1 completes: ");
+        while (true) {
+            int current = scanner.nextInt();
+            if (current == -1) {
+                break;
+            }
+            grades.addGrade(current);
+        }
+
+        System.out.println("Grade distribution: ");
+        grades.distribute();
+        System.out.println("Acceptance percentage: " + grades.acceptance());
+
     }
 }
