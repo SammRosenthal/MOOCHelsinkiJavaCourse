e6c6e2c27c92a45b8accc0dcc0a4bfd71269c73c
Sam Rosenthal <samrrosenthal@gmail.com>
2019-06-16 15:16:03 -0500

    finished finding index of lowest

--- TMCProjects/mooc-2013-OOProgrammingWithJava-PART1/week6-104.Sorting/src/Main.java
+++ TMCProjects/mooc-2013-OOProgrammingWithJava-PART1/week6-104.Sorting/src/Main.java
@@ -1,7 +1,9 @@
 
 public class Main {
     public static void main(String[] args) {
+        int[] test = {6, 3, 0, -1, 4};
 
+        indexOfTheSmallest(test);
     }
 
     public static int smallest(int[] array){
@@ -13,6 +15,20 @@ public class Main {
         }
 
         return small;
+    }
+
+    public static int indexOfTheSmallest(int[] array) {
+        int small = Integer.MAX_VALUE;
+        int index = -1;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < small) {
+
+                small = array[i];
+                index = i;
+            }
+        }
+
+        return index;
     }
 
 }
