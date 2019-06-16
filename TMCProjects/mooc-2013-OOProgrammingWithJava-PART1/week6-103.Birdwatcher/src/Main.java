5b29ca574e925d6892cffdfb04d6272bccef4dcc
Sam Rosenthal <samrrosenthal@gmail.com>
2019-06-16 14:35:12 -0500

    finished add functionality

--- TMCProjects/mooc-2013-OOProgrammingWithJava-PART1/week6-103.Birdwatcher/src/Main.java
+++ TMCProjects/mooc-2013-OOProgrammingWithJava-PART1/week6-103.Birdwatcher/src/Main.java
@@ -1,4 +1,6 @@
-public class Main {  
+import java.util.Scanner;
+
+public class Main {
 
     public static void main(String[] args) {
     // implement your program here
@@ -6,6 +8,24 @@ public class Main {
         
     // Your program should use only one Scanner object, i.e., it is allowed to call 
     // new Scanner only once. If you need scanner in multiple places, you can pass it as parameter
+
+        Scanner scanner = new Scanner(System.in);
+        Library birdLibrary = new Library();
+
+
+        while (true) {
+            if (scanner.nextLine().equals("Add")) {
+                System.out.println("Name: ");
+                String name = scanner.nextLine();
+                System.out.println("Latin Name: ");
+                String latinName = scanner.nextLine();
+                birdLibrary.add(new Bird(name, latinName));
+            } else if (scanner.next().equals("Observation")) {
+                
+            }
+
+
+        }
             
     }
 
