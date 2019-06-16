e4b577032b1a4d0f81a57e14e311026bdec1c108
Sam Rosenthal <samrrosenthal@gmail.com>
2019-06-16 15:58:59 -0500

    finished swap method

--- TMCProjects/mooc-2013-OOProgrammingWithJava-PART1/week6-104.Sorting/src/Main.java
+++ TMCProjects/mooc-2013-OOProgrammingWithJava-PART1/week6-104.Sorting/src/Main.java
@@ -1,9 +1,16 @@
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] test = {6, 3, 0, -1, 4};
+        int[] values = {3, 2, 5, 4, 8};
 
-        indexOfTheSmallest(test);
+        System.out.println( Arrays.toString(values) );
+
+        swap(values, 1, 0);
+        System.out.println( Arrays.toString(values) );
+
+        swap(values, 0, 3);
+        System.out.println( Arrays.toString(values) );
     }
 
     public static int smallest(int[] array){
@@ -30,5 +37,33 @@ public class Main {
 
         return index;
     }
+
+    public static int indexOfTheSmallestStartingFrom(int[] array, int index) {
+        int small = Integer.MAX_VALUE;
+        int dex = -1;
+        for (int i = index; i < array.length; i++) {
+            if (array[i] < small) {
+
+                small = array[i];
+                dex = i;
+            }
+        }
+
+        return dex;
+    }
+
+    public static void swap (int[] array, int index1, int index2) {
+        int one = array[index1];
+        int two = array[index2];
+
+        System.out.println(one + " one");
+        System.out.println(two + " two");
+
+        array[index1] = two;
+        array[index2] = one;
+
+    }
+
+    
 
 }
