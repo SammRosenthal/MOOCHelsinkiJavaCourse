65c889e5e96078f3f201fd4dd5a2ae0c222382eb
Sam Rosenthal <samrrosenthal@gmail.com>
2019-06-16 14:53:27 -0500

    first interation of sorting algo done

--- TMCProjects/mooc-2013-OOProgrammingWithJava-PART1/week6-104.Sorting/src/Main.java
+++ TMCProjects/mooc-2013-OOProgrammingWithJava-PART1/week6-104.Sorting/src/Main.java
@@ -1,7 +1,18 @@
 
 public class Main {
     public static void main(String[] args) {
-        // write testcode here
+
+    }
+
+    public static int smallest(int[] array){
+        int small = Integer.MAX_VALUE;
+        for (int i : array) {
+            if (i < small) {
+                small = i;
+            }
+        }
+
+        return small;
     }
 
 }
